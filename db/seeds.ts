5ac11ed5fee551fd8df34bf6d4b7901bbb9e407e
sandheep45 <budtut143@gmail.com>
2023-05-28 15:27:26 +0530

    corrected category schema

--- db/seeds.ts
+++ db/seeds.ts
@@ -7,10 +7,53 @@ import { faker } from "@faker-js/faker"
  * Probably you want to use a library like https://chancejs.com
  * to easily generate realistic data.
  */
+
+const getRandomInt = (min: number, max: number) => {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function generateRandomTags(tagCount: number, possibleTags: string[]): string {
+  const tags: string[] = []
+
+  // Generate random tags
+  for (let i = 0; i < tagCount; i++) {
+    const randomIndex = Math.floor(Math.random() * possibleTags.length)
+    const randomTag = possibleTags[randomIndex]
+    tags.push(randomTag ?? "")
+  }
+
+  // Join tags into a comma-separated string
+  return tags.join(", ")
+}
+
+function getRandomValue(options: any[]): any {
+  const randomIndex = Math.floor(Math.random() * options.length)
+  return options[randomIndex]
+}
+
 const seed = async () => {
-  // for (let i = 0; i < 5; i++) {
-  //   await db.project.create({ data: { name: "Project " + i } })
-  // }
+  const products = await db.product.findMany()
+
+  for (const product of products) {
+    await db.product.update({
+      where: { id: product.id },
+      data: {
+        Variant: {
+          create: {
+            price: parseInt(faker.commerce.price()),
+            sku: getRandomInt(1000000000, 9999999999),
+            description: faker.commerce.productDescription(),
+            inWareHouseQuantity: getRandomInt(0, 100),
+            onSelfQuantity: getRandomInt(0, 100),
+            taxPercent: getRandomInt(0, 100),
+            barcode: getRandomInt(1000000000000, 9999999999999),
+          },
+        },
+      },
+    })
+  }
 }
 
 export default seed
