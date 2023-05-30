33b6db2aeb5bb12b605eb7d637fc6f707b08e56a
sandheep45 <budtut143@gmail.com>
2023-05-30 17:36:32 +0530

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,7 @@ import { BlitzPage } from "@blitzjs/auth"
 import Layout from "@src/core/layouts/Layout"
 
 const Index: BlitzPage = () => {
-  console.log(process.env.VERCEL_URL, "normal")
-  console.log(process.env.NEXT_PUBLIC_VERCEL_URL, "next_public")
+  console.log(process.env)
   return (
     <Layout title="Dashboard">
       <a href="/api/auth/google">Log In With Google</a>
