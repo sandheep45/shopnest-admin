70ac9fdd65b2a91f424686bc00ba75b4b3407d27
sandheep45 <budtut143@gmail.com>
2023-08-19 20:27:08 +0530

    restructured listing pages

--- src/app/(dashboard)/categories/components/LoadingTableSkeleton.tsx
+++ src/app/(dashboard)/categories/components/LoadingTableSkeleton.tsx
@@ -1,66 +1,86 @@
-import React from 'react';
+import React from "react";
 
-import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
-import { Skeleton } from '@/components/ui/skeleton';
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { Skeleton } from "@/components/ui/skeleton";
 import {
-    Table, TableBody, TableCell, TableHead, TableHeader, TableRow
-} from '@/components/ui/table';
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const myArray: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const CategoryLoadingTable = () => {
   return (
-    <ScrollArea className="h-[calc(100vh-220px)] w-full overflow-x-auto">
-      <Table className="w-full">
-        <TableHeader>
-          <TableRow>
-            <TableHead>
-              <Skeleton className="h-5 w-5 rounded-md" />
-            </TableHead>
-            <TableHead>
-              <Skeleton className="h-5 w-32 rounded-md mx-auto" />
-            </TableHead>
-            <TableHead>
-              <Skeleton className="h-5 w-32 rounded-md mx-auto" />
-            </TableHead>
-            <TableHead>
-              <Skeleton className="h-5 w-24 rounded-md mx-auto" />
-            </TableHead>
-            <TableHead></TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {myArray.map((item) => (
-            <TableRow key={item}>
-              <TableCell>
-                <Skeleton className="h-5 w-5 rounded-md" />
-              </TableCell>
-              <TableCell>
-                <div className="flex items-center space-x-4">
-                  <Skeleton className="h-12 w-12 rounded-full" />
-                  <div className="space-y-2">
-                    <Skeleton className="h-4 w-[200px]" />
-                    <Skeleton className="h-4 w-[150px]" />
-                  </div>
-                </div>
-              </TableCell>
-              <TableCell className="flex flex-col gap-3 items-center justify-center">
-                <Skeleton className="h-4 w-64 lg:w-[550px]" />
-                <Skeleton className="h-4 w-64 lg:w-[550px]" />
-                <Skeleton className="h-4 w-64 lg:w-[350px]" />
-              </TableCell>
-              <TableCell>
-                <Skeleton className="h-4 w-[100px] mx-auto" />
-              </TableCell>
-              <TableCell>
-                <Skeleton className="h-4 w-[100px] mx-auto" />
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <ScrollBar orientation="horizontal" />
-    </ScrollArea>
+    <div className="flex w-full flex-col gap-6">
+      <h3 className="text-2xl">Categories Listing</h3>
+      <div className="relative flex w-full flex-col gap-8 rounded-md bg-[#f5f8fa] p-5 dark:bg-[#1e1e2d]">
+        <div className="flex items-center justify-between">
+          <Skeleton className="h-8 w-56 rounded-md" />
+
+          <div className="flex items-center justify-center gap-3">
+            <Skeleton className="h-8 w-24 rounded-md" />
+
+            <Skeleton className="h-8 w-24 rounded-md" />
+          </div>
+        </div>
+
+        <ScrollArea className="h-[calc(100vh-220px)] w-full overflow-x-auto">
+          <Table className="w-full">
+            <TableHeader>
+              <TableRow>
+                <TableHead>
+                  <Skeleton className="h-5 w-5 rounded-md" />
+                </TableHead>
+                <TableHead>
+                  <Skeleton className="mx-auto h-5 w-32 rounded-md" />
+                </TableHead>
+                <TableHead>
+                  <Skeleton className="mx-auto h-5 w-32 rounded-md" />
+                </TableHead>
+                <TableHead>
+                  <Skeleton className="mx-auto h-5 w-24 rounded-md" />
+                </TableHead>
+                <TableHead></TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {myArray.map((item) => (
+                <TableRow key={item}>
+                  <TableCell>
+                    <Skeleton className="h-5 w-5 rounded-md" />
+                  </TableCell>
+                  <TableCell>
+                    <div className="flex items-center space-x-4">
+                      <Skeleton className="h-12 w-12 rounded-full" />
+                      <div className="space-y-2">
+                        <Skeleton className="h-4 w-[200px]" />
+                        <Skeleton className="h-4 w-[150px]" />
+                      </div>
+                    </div>
+                  </TableCell>
+                  <TableCell className="flex flex-col items-center justify-center gap-3">
+                    <Skeleton className="h-4 w-64 lg:w-[550px]" />
+                    <Skeleton className="h-4 w-64 lg:w-[550px]" />
+                    <Skeleton className="h-4 w-64 lg:w-[350px]" />
+                  </TableCell>
+                  <TableCell>
+                    <Skeleton className="mx-auto h-4 w-[100px]" />
+                  </TableCell>
+                  <TableCell>
+                    <Skeleton className="mx-auto h-4 w-[100px]" />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
+      </div>
+    </div>
   );
 };
 export default CategoryLoadingTable;
--- src/app/(dashboard)/users/components/LoadingTableSkeleton.tsx
+++ src/app/(dashboard)/users/components/LoadingTableSkeleton.tsx
@@ -1,81 +1,95 @@
-import React from 'react';
+import React from "react";
 
-import { Button } from '@/components/ui/button';
-import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
-import { Skeleton } from '@/components/ui/skeleton';
+import { Button } from "@/components/ui/button";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { Skeleton } from "@/components/ui/skeleton";
 import {
-    Table, TableBody, TableCell, TableHead, TableHeader, TableRow
-} from '@/components/ui/table';
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const myArray: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const UserLoadingTableSkeleton = () => {
   return (
-    <div className="flex flex-col w-full">
-      <ScrollArea className="h-[calc(100vh-220px)] w-full overflow-x-auto">
-        <Table className="w-full">
-          <TableHeader>
-            <TableRow>
-              <TableHead>
-                <Skeleton className="h-5 w-5 rounded-md" />
-              </TableHead>
-              <TableHead>
-                <Skeleton className="h-5 w-32 rounded-md mx-auto" />
-              </TableHead>
-              <TableHead>
-                <Skeleton className="h-5 w-32 rounded-md mx-auto" />
-              </TableHead>
-              <TableHead>
-                <Skeleton className="h-5 w-20 rounded-md mx-auto" />
-              </TableHead>
-              <TableHead>
-                <Skeleton className="h-5 w-24 rounded-md mx-auto" />
-              </TableHead>
-              <TableHead>
-                <Skeleton className="h-5 w-24 rounded-md mx-auto" />
-              </TableHead>
-              <TableHead>
-                <Skeleton className="h-5 w-24 rounded-md mx-auto" />
-              </TableHead>
-              <TableHead></TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {myArray.map((item) => (
-              <TableRow key={item}>
-                <TableCell>
+    <div className="flex w-full flex-col gap-6">
+      <h3 className="text-2xl">Users Listing</h3>
+      <div className="relative flex w-full flex-col gap-8 rounded-md bg-[#f5f8fa] p-5 dark:bg-[#1e1e2d]">
+        <div className="flex items-center justify-between">
+          <Skeleton className="h-8 w-56 rounded-md" />
+
+          <div className="flex items-center justify-center gap-3">
+            <Skeleton className="h-8 w-24 rounded-md" />
+
+            <Skeleton className="h-8 w-24 rounded-md" />
+          </div>
+        </div>
+
+        <ScrollArea className="h-[calc(100vh-220px)] w-full overflow-x-auto">
+          <Table className="w-full">
+            <TableHeader>
+              <TableRow>
+                <TableHead>
                   <Skeleton className="h-5 w-5 rounded-md" />
-                </TableCell>
-                <TableCell>
-                  <div className="flex items-center space-x-4">
-                    <Skeleton className="h-12 w-12 rounded-full" />
-                    <Skeleton className="h-4 w-[160px]" />
-                  </div>
-                </TableCell>
-                <TableCell>
-                  <Skeleton className="h-4 w-64 lg:w-[200px]" />
-                </TableCell>
-                <TableCell>
-                  <Skeleton className="h-4 w-[80px] mx-auto" />
-                </TableCell>
-                <TableCell>
-                  <Skeleton className="h-4 w-[80px] mx-auto" />
-                </TableCell>
-                <TableCell>
-                  <Skeleton className="h-4 w-[200px] mx-auto" />
-                </TableCell>
-                <TableCell>
-                  <Skeleton className="h-4 w-[100px] mx-auto" />
-                </TableCell>
+                </TableHead>
+                <TableHead>
+                  <Skeleton className="mx-auto h-5 w-32 rounded-md" />
+                </TableHead>
+                <TableHead>
+                  <Skeleton className="mx-auto h-5 w-32 rounded-md" />
+                </TableHead>
+                <TableHead>
+                  <Skeleton className="mx-auto h-5 w-20 rounded-md" />
+                </TableHead>
+                <TableHead>
+                  <Skeleton className="mx-auto h-5 w-24 rounded-md" />
+                </TableHead>
+                <TableHead>
+                  <Skeleton className="mx-auto h-5 w-24 rounded-md" />
+                </TableHead>
+                <TableHead>
+                  <Skeleton className="mx-auto h-5 w-24 rounded-md" />
+                </TableHead>
+                <TableHead></TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-        <ScrollBar orientation="horizontal" />
-      </ScrollArea>
-      <div className="flex w-full justify-between">
-        <Button>Previous</Button>
-        <Button>Next</Button>
+            </TableHeader>
+            <TableBody>
+              {myArray.map((item) => (
+                <TableRow key={item}>
+                  <TableCell>
+                    <Skeleton className="h-5 w-5 rounded-md" />
+                  </TableCell>
+                  <TableCell>
+                    <div className="flex items-center space-x-4">
+                      <Skeleton className="h-12 w-12 rounded-full" />
+                      <Skeleton className="h-4 w-[160px]" />
+                    </div>
+                  </TableCell>
+                  <TableCell>
+                    <Skeleton className="h-4 w-64 lg:w-[200px]" />
+                  </TableCell>
+                  <TableCell>
+                    <Skeleton className="mx-auto h-4 w-[80px]" />
+                  </TableCell>
+                  <TableCell>
+                    <Skeleton className="mx-auto h-4 w-[80px]" />
+                  </TableCell>
+                  <TableCell>
+                    <Skeleton className="mx-auto h-4 w-[200px]" />
+                  </TableCell>
+                  <TableCell>
+                    <Skeleton className="mx-auto h-4 w-[100px]" />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
       </div>
     </div>
   );
